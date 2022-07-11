1305a0bb1e594d21b4dc084ab14e3da8862d55f8
Charan <charanlucky6143@gmail.com>
2022-07-11 19:14:25 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import SeasonDisply from './SeasonDisplay';
+import Loader from './Loader';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position),
+//         (err) => console.log(err)
+//     );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//     return <div>Hello React</div>;
+// };
+
+class App extends React.Component {
+    state = { lat: null, errMsg: '' }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errMsg: err.message })
+        );
+    }
+
+    renderContent() {
+        if (this.state.errMsg && !this.state.lat) {
+            return <div>Error Message:- {this.state.errMsg}</div>;
+        } else if (this.state.lat) {
+            return (
+                <div>
+                    <SeasonDisply lat={this.state.lat} />
+                </div>
+            );
+        } else {
+            return <Loader message="Please accept loaction request" />;
+        }
+    }
+
+    render() {
+        return <div>{this.renderContent()}</div>;
+    }
+};
+
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+root.render(<App />);
